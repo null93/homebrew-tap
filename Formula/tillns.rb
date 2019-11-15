e8a371dfcb8cb7c1490a51ebe56ef1a72870f097
Rafael Grigorian <me@raffi.io>
2019-11-14 18:02:09 -0600

    Released tillns@1.1.0

--- Formula/tillns.rb
+++ Formula/tillns.rb
@@ -2,17 +2,20 @@ class Tillns < Formula
 
 	desc "Switch faster between tiller namespaces in helm"
 	homepage "https://github.com/null93/tillns"
-	url "https://github.com/null93/tillns/archive/1.0.0.tar.gz"
-	sha256 "cdbfe4300de99495763e17435f03e1aed8437add81ead011f7e73ebc37e4c248"
-	version "1.0.0"
+	url "https://github.com/null93/tillns/archive/1.1.0.tar.gz"
+	sha256 "b8704971af0ccf434c1596fe85c6ca6918006acdcc96485302c14e8072198c5e"
+	version "1.1.0"
 
 	def install
 		bin.install "bin/tillns"
 	end
 
 	def caveats
-		"Alias helm command to go through tillns via --exec flag\n\n" +
-		"    echo -e \"alias helm='tillns --exec'\" >> ~/.bash_profile\n\n"
+		"Alias helm command to go through tillns\n\n" +
+		"    $ echo -e \"alias helm='tillns'\" >> ~/.bash_profile\n\n" +
+		"Use helm normally, change tiller namespace with kubectx/kubens\n\n" +
+		"    # Passing -- will disable this program and run helm normally\n" +
+		"    $ helm -- ls -a \n\n"
 	end
 
 end
