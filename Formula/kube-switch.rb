666675b8dde3e44d5d2bd0adf6d1fafa99ea1d37
Rafael Grigorian <me@raffi.io>
2020-08-04 22:26:50 -0500

    Released kube-switch@1.0.1

--- Formula/kube-switch.rb
+++ Formula/kube-switch.rb
@@ -2,14 +2,14 @@ class KubeSwitch < Formula
 
 	desc "Switch between Kubernetes context & namespace using an interactive menu."
 	homepage "https://github.com/null93/kube-switch"
-	url "https://github.com/null93/kube-switch/archive/1.0.0.tar.gz"
-	sha256 "65722de9e6be0230d019b1b8d48ccb4e83707ca5b63c2a2651cd1daad54d5811"
-	version "1.0.0"
+	url "https://github.com/null93/kube-switch/archive/1.0.1.tar.gz"
+	sha256 "c5256b166109f2d88d29a1c43f89f61eaa9cad7a8720578af83c0cd7d64bd0ae"
+	version "1.0.1"
 
 	depends_on "python3"
 
 	resource "kubernetes" do
-		url "https://files.pythonhosted.org/packages/60/3f/2fef94fb65e8f94d768356e5fb9be222d18027e6167ccc65e2090917a771/kubernetes-11.0.0.tar.gz"
+		url "https://files.pythonhosted.org/packages/60/3f/2fef94fb65e8f94d768356e5fb9be222d18027e6167ccc65e2090917a771/kubernetes-11.0.1.tar.gz"
 		sha256 "1a2472f8b01bc6aa87e3a34781f859bded5a5c8ff791a53d889a8bd6cc550430"
 	end
 
