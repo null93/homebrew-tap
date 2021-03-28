578f9c60b2dee06e2c7f23d7e82e1f7f784357c8
Rafael Grigorian <me@raffi.io>
2021-03-28 01:28:58 -0500

    Released kube-switch@2.0.1

--- Formula/kube-switch.rb
+++ Formula/kube-switch.rb
@@ -2,9 +2,9 @@ class KubeSwitch < Formula
 
 	desc "Switch between Kubernetes context & namespace using an interactive menu."
 	homepage "https://github.com/null93/kube-switch"
-	url "https://github.com/null93/kube-switch/releases/download/2.0.0/kube-switch-2.0.0-darwin.tar.gz"
-	sha256 "7fca6886aa37772d795f07b45ccd7ca0c89805ce8e50041be9dfe8291ea19cd7"
-	version "2.0.0"
+	url "https://github.com/null93/kube-switch/releases/download/2.0.1/kube-switch-2.0.1-darwin.tar.gz"
+	sha256 "4faabe3902cce0476f90f7b28a6aba574156ab6f5ff9738ac34a6debc6ed5b2f"
+	version "2.0.1"
 
 	def install
 		bin.install "kube-switch_darwin_amd64" => "kube-switch"
@@ -16,7 +16,8 @@ class KubeSwitch < Formula
 
 	def caveats
 		"Optionally bind program to CTRL+K & ideally append to .bash_profile\n\n" +
-		"    $ bind -x '\"\\C-k\":\"kube-switch\"'\n\n"
+		"    $ bind -x '\"\\C-k\":\"kube-switch\"'\n" +
+		"    $ bind -x '\"\\C-n\":\"kube-switch -c\"'\n\n"
 	end
 
 end
